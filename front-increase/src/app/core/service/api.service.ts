77cbb1a2231b852f501f982b3c6f55c52b758ede
Mykola <developersame@gmail.com>
2019-10-05 22:34:48 +0300

    Add login

---
+++ front-increase/src/app/core/service/api.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http
+      .get(`${environment.url}${path}`, { params });
+  }
+
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .put(`${environment.url}${path}`, body);
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .post(`${environment.url}${path}`, body);
+  }
+
+  delete(path): Observable<any> {
+    return this.http
+      .delete(`${environment.url}${path}`);
+  }
+}
--- front-increase/src/app/login/login/login.component.ts
+++ front-increase/src/app/login/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from 'src/app/core/service/api.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
   }
 
+  getUserById() {
+    this.apiService.get('user/1').subscribe((e)=> console.log(e, 333333333))
+  }
 }
